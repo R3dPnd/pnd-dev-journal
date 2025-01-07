528641806f7a4f9dcd7a8f3fb6d1ee8aaeda447d
Preston Harms <prestonharms@microsoft.com>
2025-01-06 22:18:29 -0800

    Add SCSS styling, header component, and theme management functionality

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Markdown from 'react-markdown';
-import axios from 'axios';
 import { getMarkdown } from './common/services/mark-down-service';
+import Header from './common/components/header/header';
 
 function App() {
   let markdown = "https://github.com/Medium/medium-api-docs/blob/master/README.md";
@@ -17,15 +14,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Markdown>
-          {markdown}
-          </Markdown>
-      </header>
+      <Header/>
     </div>
   );
 }
---
+++ client-app/src/common/components/header/header.tsx
@@ -0,0 +1,9 @@
+import "./header.scss";
+
+export default function Header() {
+    return (
+        <header className="pnd-header">
+            <h1>Dev Journal</h1>
+        </header>
+    );
+}
